315ce5fa72f4985ab7108e1737c72df372dc055f
Stephen Madalo <stephenmadalo89@gmail.com>
2023-08-08 14:09:03 -0400

    init commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            
+            
+            account.Deposit(90);
+            var bal = account.GetBalance();
+            Console.WriteLine($"Your account balance is: {bal}");
+
+
         }
     }
 }
